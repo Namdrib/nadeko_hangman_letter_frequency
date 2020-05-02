2c75a9da7cf2b29c9229d767d1c3d6c5b61321e2
Namdrib <ph0fgazfl4j1@opayq.com>
2020-05-02 19:22:28 +0930

    fix json acquisition url

--- acquire_json.sh
+++ acquire_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/Kwoth/NadekoBot/1.4/src/NadekoBot/data/hangman3.json -O "hangman_all.json"
+wget https://gitlab.com/Kwoth/nadekobot/-/blob/global/src/NadekoBot/data/hangman.json -O "hangman_all.json"
